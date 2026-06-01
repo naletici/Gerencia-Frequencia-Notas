09ead597806f3b1869a63e0b2a8462cad6bc539a
Ana Mesquita <naletici1101@gmail.com>
2026-05-31 23:45:02 -0300

    Create abstract class Avaliacao with validation

---
+++ Avaliacao.java
@@ -0,0 +1,35 @@
+public abstract class Avaliacao {
+    private String nomeAvaliacao;
+    private double notaObtida;
+
+    public Avaliacao(String nomeAvaliacao, double notaObtida) {
+        this.nomeAvaliacao = nomeAvaliacao;
+        validarNota(notaObtida);
+        this.notaObtida = notaObtida;
+    }
+  
+    public abstract double calcularNotaDefinitiva();
+
+    protected void validarNota(double nota) {
+        if (nota < 0.0 || nota > 10.0) {
+            throw new NotaInvalidaException("A nota deve estar entre 0.0 e 10.0.");
+        }
+    }
+
+    public String getNomeAvaliacao() {
+        return nomeAvaliacao;
+    }
+
+    public void setNomeAvaliacao(String nomeAvaliacao) {
+        this.nomeAvaliacao = nomeAvaliacao;
+    }
+
+    public double getNotaObtida() {
+        return notaObtida;
+    }
+
+    public void setNotaObtida(double notaObtida) {
+        validarNota(notaObtida);
+        this.notaObtida = notaObtida;
+    }
+}
