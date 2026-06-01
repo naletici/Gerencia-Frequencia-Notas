4882fe28797875904dc243b4ad0f4acea55b3fbc
Ana Mesquita <naletici1101@gmail.com>
2026-05-31 23:41:44 -0300

    Create abstract class MembroAcademico

---
+++ MembroAcademico.java
@@ -0,0 +1,25 @@
+public abstract class MembroAcademico {
+    private String nome;
+    private String matricula;
+
+    public MembroAcademico(String nome, String matricula) {
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+}
