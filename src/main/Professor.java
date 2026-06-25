2ae0d9f121e48ba4bcef6a16efe2693fec5f71fb
naletici <naletici1101@gmail.com>
2026-06-25 00:53:10 -0300

    Montando Professor

--- src/main/Professor.java
+++ src/main/Professor.java
@@ -1,5 +1,8 @@
 package src.main;
 
 public class Professor extends MembroAcademico implements RelatorioGeravel {
-    
+    @Override
+    public void gerarRelatorio() {
+        System.out.println("Professor: " + getNome() + " (Matrícula: " + getMatricula() + ")");
+    }
 }
