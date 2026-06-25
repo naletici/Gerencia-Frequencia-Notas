d5f0ff66c8406b71ea52baf530966b79ca7e6879
naletici <naletici1101@gmail.com>
2026-06-24 23:15:48 -0300

    Construção de Menu na Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,4 +8,39 @@ public class Main {
     public static void main(String[] a) {
         
     }
+
+    private static void exibirMenu() {
+        System.out.println("================================================");
+        System.out.println("        Gerenciador de Frequência e Notas       ");
+        System.out.println("================================================");
+        System.out.println("[1]. Cadastrar Matéria.");
+        System.out.println("[2]. Cadastrar Professor.");
+        System.out.println("[3]. Cadastrar Aluno.");
+        System.out.println("[4]. Lançar Notas.");
+        System.out.println("[5]. Lançar Faltas.");
+        System.out.println("[6]. Gerar Relatório.");
+        System.out.println("[7]. Sair.");
+        System.out.println("Escolha uma opção.");
+    }
+
+    private static void escolherOpcao(int opcao){
+        switch(opcao) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                System.out.println("\nEncerrando Gerenciador...");
+                System.out.println("\nEncerrado com sucesso!");
+                break;
+        }
+    }
 }
