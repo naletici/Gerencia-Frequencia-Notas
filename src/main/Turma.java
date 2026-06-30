19a03a5e863d0b1b719493220a40e78df35d4ade
naletici <naletici1101@gmail.com>
2026-06-30 15:59:39 -0300

    Preenchendo Turma

--- src/main/Turma.java
+++ src/main/Turma.java
@@ -1,4 +1,23 @@
 package src.main;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Turma {
-    
+    private Materia materia;
+    private Professor professor;
+    private List<Aluno> listaAlunos = new ArrayList<>();
+
+    public void matricularAluno(Aluno aluno) {
+        if (aluno != null) {
+            listaAlunos.add(aluno);
+        }
+    }
+
+    public boolean removerAluno(int indice) {
+        if (indice >= 0 && indice < listaAlunos.size()) {
+            listaAlunos.remove(indice);
+            return true;
+        }
+        return false;
+    }
 }
