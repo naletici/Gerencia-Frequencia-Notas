8fc6c465215f451f4fa531297565fffef505e5e7
naletici <naletici1101@gmail.com>
2026-06-30 16:47:26 -0300

    Preenchimento de TrabPrat

--- src/main/TrabalhoPratico.java
+++ src/main/TrabalhoPratico.java
@@ -1,4 +1,22 @@
 package src.main;
 public class TrabalhoPratico extends Avaliacao {
-    
+    private double peso;
+
+    public TrabalhoPratico(String nomeAvaliacao, double notaObtida, double peso) {
+        super(nomeAvaliacao, notaObtida);
+        this.peso = peso;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    @Override
+    public double calcularNotaTotal() {
+        return getNotaObtida() * peso;
+    }
 }
