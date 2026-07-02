3ca2100fdb956ceafd30f69f6b8094228d56cbd4
naletici <naletici1101@gmail.com>
2026-07-02 00:06:23 -0300

    javadoc Prova.java

--- src/main/Prova.java
+++ src/main/Prova.java
@@ -1,9 +1,21 @@
 package src.main;
+/**
+ * Classe que representa uma prova
+ * Prova
+ */
 public class Prova extends Avaliacao {
+    /**
+     * Construtor da classe Prova
+     * @param nomeAvaliacao O nome da prova
+     * @param notaObtida A nota obtida na prova
+     */
     public Prova(String nomeAvaliacao, double notaObtida) {
         super(nomeAvaliacao, notaObtida);
     }
-
+    /**
+     * Método para calcular a nota total da prova
+     * @return A nota obtida na prova
+     */
     @Override
     public double calcularNotaTotal() {
         return getNotaObtida();
