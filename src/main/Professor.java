b9d27d923a897ddbb4648f1f638f64788cb50133
naletici <naletici1101@gmail.com>
2026-07-01 23:46:00 -0300

    javadoc Professor.java

--- src/main/Professor.java
+++ src/main/Professor.java
@@ -1,6 +1,12 @@
 package src.main;
-
+/**
+ * Classe que representa um professor com a capacidade de gerar seu próprio relatório
+ * Professor
+ */
 public class Professor extends MembroAcademico implements RelatorioGeravel {
+    /**
+     * Método para gerar o relatório do professor
+     */
     @Override
     public void gerarRelatorio() {
         System.out.println("Professor: " + getNome() + " (Matrícula: " + getMatricula() + ")");
