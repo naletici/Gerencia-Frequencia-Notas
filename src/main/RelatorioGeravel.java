3cc85d178e59cbd80ddd12134d3ac8f5e2ca483d
naletici <naletici1101@gmail.com>
2026-07-01 23:27:20 -0300

    Javadoc RelatórioGeravel

--- src/main/RelatorioGeravel.java
+++ src/main/RelatorioGeravel.java
@@ -1,4 +1,10 @@
 package src.main;
+/**
+ * Interface feita para possibilitar que um relatório seja gerável
+ */
 public interface RelatorioGeravel {
+/**
+ * Método que possibilita gerar o relatório
+ */
     void gerarRelatorio();
 }
