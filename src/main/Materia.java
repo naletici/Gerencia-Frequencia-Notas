f17edfc8f9d6525995da4c434254e93ac1af828d
naletici <naletici1101@gmail.com>
2026-07-02 00:15:50 -0300

    javadoc Materia.java

--- src/main/Materia.java
+++ src/main/Materia.java
@@ -1,21 +1,36 @@
 package src.main;
-
+/**
+ * Classe que representa uma matéria e a sua carga horária total
+ * Materia
+ */
 public class Materia {
     private String nomeMateria;
     private int cargaHorariaTotal;
-
+    /**
+     * Método para obter o nome da matéria
+     * @return O nome da matéria
+     */
     public String getNomeMateria() {
         return nomeMateria;
     }
-
+    /**
+     * Método para definir o nome da matéria
+     * @param nomeMateria O nome a ser atribuído
+     */
     public void setNomeMateria(String nomeMateria){
         this.nomeMateria = nomeMateria;
     }
-
+    /**
+     * Método para obter a carga horária total da matéria
+     * @return A carga horária total, em horas
+     */
     public int getCargaHorariaTotal() {
         return cargaHorariaTotal;
     }
-
+    /**
+     * Método para definir a carga horária total da matéria
+     * @param cargaHorariaTotal A carga horária a ser atribuída, em horas
+     */
     public void setCargaHorariaTotal(int cargaHorariaTotal) {
         this.cargaHorariaTotal = cargaHorariaTotal;
     }
